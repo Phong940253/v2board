f1d8b435a27b6a3e00047810863778b40949033b
Phong <phong940253@gmail.com>
2023-11-14 22:53:42 +0700

    update send telegram

--- app/Console/Commands/SendTraffic.php
+++ app/Console/Commands/SendTraffic.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Models\StatServer;
 use App\Services\TelegramService;
 use Illuminate\Console\Command;
-use App\Http\Controllers\Admin\StatController;
+use App\Http\Controllers\V1\Admin\StatController;
 
 class SendTraffic extends Command
 {
@@ -47,19 +47,21 @@ class SendTraffic extends Command
     public function getStatServer() {
         $statcontroller = new StatController;
         $data = $statcontroller->getServerLastRankArray();
-        $result = "🚥 Báo cáo dung lượng VPN sử dụng hôm qua\n🌞 Powered By: Nguyễn Văn Phong\n🌞 Website: https://phong940253.tk\n";
+        $result = "🚥 Báo cáo dung lượng VPN sử dụng hôm qua\n🌞 Powered By: Nguyễn Văn Phong\n🌞 Website: https://phongnguyen.tech\n";
         if ($data) {
+            $statistics = $data['data'];
             $telegramService = new TelegramService();
-            foreach ($data as $row) {
+            foreach ($statistics as $row) {
                 if (array_key_exists('server_name', $row)) {
                     $up_format = number_format($row['u'] / pow(1024, 3), 2);
                     $down_format = number_format($row['d'] / pow(1024, 3), 2);
                     $total_format = number_format($row['total'], 2);
                     $result .= "{$row['server_name']}: Tải lên {$up_format}Gb, Tải xuống {$down_format}Gb, Tất cẳ {$total_format}Gb\n";
                 }
+                // $this->info($row['server_id'], $row['u'], $row['d'], $row['total']);
             }
             $telegramService->sendMessage('-1001712589741' ,$result);
-//            $this->info($result);
+            // $this->info($result);
         }
     }
 }
